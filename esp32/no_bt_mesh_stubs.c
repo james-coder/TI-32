ac8b0f495ae52e1fa393aad4ae5b3c2953383056
james-coder <james-coder@users.noreply.github.com>
2026-02-04 20:32:37 -0700

    Make BT stubs independent of esp_bt.h
    Avoid including esp_bt.h in no_bt_mesh_stubs.c so builds succeed when BT headers are excluded from custom libs.

--- esp32/no_bt_mesh_stubs.c
+++ esp32/no_bt_mesh_stubs.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
 
-#include "esp_bt.h"
 #include "esp_err.h"
+
+typedef int esp_bt_mode_t;
 
 __attribute__((weak)) esp_err_t esp_bt_controller_mem_release(esp_bt_mode_t mode) {
   (void)mode;
